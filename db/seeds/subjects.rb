8943cd39ba25e60fb3d2ca713c940f5ac027023a
atosbucket <tiggonking@gmail.com>
2024-03-19 19:19:50 -0700

    addtional files in migration and seed

---
+++ db/seeds/subjects.rb
@@ -0,0 +1,6 @@
+Subject.create(name: 'Physics')
+Subject.create(name: 'Mathematics')
+Subject.create(name: 'Chemistry')
+Subject.create(name: 'Psychology')
+Subject.create(name: 'Geography')
+puts "generated subjects seed successfully!"
